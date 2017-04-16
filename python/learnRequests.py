ad0fb934b58c08ec3c560715b512986a4818bd0b
wkx <wkx@MyServer.ComputerLover.help>
2017-04-17 07:21:32 +0800

    爬虫学习

--- python/learnRequests.py
+++ python/learnRequests.py
@@ -1,9 +1,14 @@
 
 import requests
- 
-r = requests.get(url='http://keet.dididown.co/')    # 最基本的GET请求
+from lxml import etree
+
+url = 'http://keet.dididown.co/'
+r = requests.get(url)
+tree = etree.HTML(r.text)
+
 print(r.status_code)    # 获取返回状态
 print(r.url)
 print(r.encoding)
 r.encoding='utf-8'
 print(r.text)   #打印解码后的返回数据
+print(tree)
