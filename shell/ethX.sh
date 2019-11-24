3c75bd735cd6fe35df7ced56905d4255a8423677
wkx <wkx@MyServer.ComputerLover.help>
2019-11-24 18:14:53 +0800

    erase br0

--- shell/ethX.sh
+++ shell/ethX.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-ifup ethX1 ethX2 ethX3 ethX4 &&
+depmod -a
+modprobe -v r8169
+#
+ifconfig ethX1 ethX2 ethX3 ethX4 down &&
+ifconfig ethX1 ethX2 ethX3 ethX4 up &&
 ifconfig ethX1 192.168.0.123 netmask 255.255.255.0
 ifconfig ethX2 192.168.0.124 netmask 255.255.255.0
 ifconfig ethX3 192.168.0.125 netmask 255.255.255.0
 ifconfig ethX4 192.168.0.126 netmask 255.255.255.0
-#
-brctl addbr br0
-brctl addif br0 eth2
-ifconfig br0 192.168.0.130 up
 #OpenBSD
 brctl addbr br1
 brctl addif br1 ethX1
