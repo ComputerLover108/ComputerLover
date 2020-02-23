97dd7c4296a6fbd9357c865c59f922816bb60c58
unknown <ComputerLove108>
2020-02-23 20:07:02 +0800

    multiprocess story_crawling

--- python/NCP.py
+++ python/NCP.py
@@ -1,31 +1,25 @@
-# import requests
-# import json 
-
-# # url = "https://ncov.dxy.cn/ncovh5/view/pneumonia?scene=2&clicktime=1579579384&enterid=1579579384&from=timeline&isappinstalled=0"
-# url = "https://lab.isaaclin.cn/nCoV/api/area?latest=1"
-# r = requests.get(url=url)    # 最基本的GET请求
-# print(r.status_code, r.reason)
-# print('GET请求', r.text)
-
-# 自定义headers请求
-# ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) ' \
-#      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77' \
-#      ' Safari/537.36'
-# headers = {'User-Agent': ua}
-# response = requests.get(url=url, headers=headers)
-# json_reads = json.loads(response.text)
-# ALL=json_reads["results"]
-# print(ALL)
-
 import requests
 import json
 import time, datetime
-import xlwt
 import threading
 import os
 import psycopg2
 import csv
-from io import StringIO
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(level = logging.INFO)
+handler = logging.FileHandler('ncp.log')
+handler.setLevel(logging.INFO)
+formatter = "%(message)s %(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d "
+formatter = logging.Formatter(formatter)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+# console = logging.StreamHandler()
+# console.setLevel(logging.INFO)
+# logger.addHandler(console)
+
+
 
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) ' \
      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77' \
@@ -61,14 +55,15 @@ class MyPostgreSQL:
         try:
             self.cursor.execute(SQL)
         except Exception as e:
-            print(e)
+            logger.info(e)
             self.conn.rollback()
             self.cursor.execute(SQL)
         self.conn.commit()
 
-def DBSave(records,f):
+def DBSave(records):
     SQL = '''
     CREATE TABLE IF NOT EXISTS NCP (
+        id serial,
         "currentConfirmedCount"  integer,
         "confirmedCount"  integer,
         "suspectedCount"  integer,
@@ -95,13 +90,42 @@ def DBSave(records,f):
     );
     '''
     psql = MyPostgreSQL(dbname='HLD',user='operator',password='5302469',host='127.0.0.1',port='2012')
+    # psql.execute('DROP TABLE IF EXISTS "NCP";')
     psql.execute(SQL)
-    fieldnames = records[0].keys()
-    print(SQL)    
-    print(fieldnames)
-    # psql.cursor.copy_from(f, "NCP",
-    #             columns=fieldnames, 
-    #             sep=',', null='\\N', size=16384)
+    # logger.info(records)
+    # """简单实用，属于游标的对象方法"""
+    # # sql = f'insert into {name}(grid_id, data, published_at) values (%s, %s, %s);'
+    columns=tuple(records[0].keys())
+    name = 'ncp'
+    SQL = f"""insert into {name} ({','.join(columns)}) values ({','.join(['%s'] * len(columns))});"""
+    # logger.info(SQL)
+    rows = list()
+    for record in records:
+        for k,v in record.items():
+            # logger.info([k,v])
+            rows.append([k,v])
+        # for k,v in record.keys():
+        #     rows.append([k,v])
+    logger.info(rows)
+    # psql.cursor.executemany(SQL,rows)
+    # psql.conn.commit()
+    # SQL = ''
+    # psql.cursor.executemany("""INSERT INTO bar(first_name,last_name) VALUES (%(first_name)s, %(last_name)s)""", namedict)
+    # fieldnames = tuple(records[0].keys())
+    # logger.info(fieldnames)
+    # # logger.info(SQL)
+    # # for fieldname in fieldnames:    
+    # #     logger.info(fieldname)
+    # # psql.cursor.copy_from(f, "NCP",columns=fieldnames,sep=',', null='\\N', size=16384)
+    # filename = 'NCP.csv'
+    # with open(filename, 'w', newline='') as csvfile:
+    #     fieldnames = records[0].keys()
+    #     logger.info(fieldnames)
+    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    #     writer.writeheader()
+    #     writer.writerows(records)
+
+    # psql.cursor.copy_from(csvfile, 'ncp',columns=fieldnames,sep=',', size=16384)
 
 def cxk():
     url = "https://lab.isaaclin.cn/nCoV/api/overall"
@@ -111,78 +135,14 @@ def cxk():
     filename = 'NCP.json'
     with open(filename,'w') as f:
         json.dump(records,f,ensure_ascii=False)
-    # print(type(records))
-    filename = 'NCP.csv'
-    with open(filename, 'w', newline='') as csvfile:
-        fieldnames = records[0].keys()
-        print(fieldnames)
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        for record in records:
-            writer.writerow(record)
-
-    DBSave(records,csvfile)
+    # logger.info(type(records))
+    DBSave(records)
     
 
     newTime = datetime.datetime.fromtimestamp(records[0]["updateTime"]/1000.0).strftime("%Y-%m-%d-%H")
  
-    print(newTime+"统计完成")
+    logger.info(newTime+"统计完成")
 
-    #创建工作博
-    # wb  =xlwt.Workbook(encoding='utf-8')
- 
- 
-    # #括号内参数为表名 ws 为各省份表
-    # ws = wb.add_sheet('%s时新型肺炎统计表'%newTime)
- 
-    # #括号内参数为表名 wk 为各市区表
-    # wk = wb.add_sheet('%s时新型肺炎各省份统计表'%newTime)
- 
-    # provinceName=[]#省份
-    # cityName=[]#下属城市
-    # confirmedCount=[]#确诊人数
-    # suspectedCount=[]#疑似病例
-    # curedCount=[]#治愈人数
-    # deadCount=[]#死亡人数
- 
-    # ws.write(0,0,label = '国家')
-    # ws.write(0,1,label = '省份')
-    # ws.write(0,2,label = '死亡人数')
-    # ws.write(0,3,label = '治愈人数')
-    # ws.write(0,4,label = '确诊人数')
-    # ws.write(0,5,label = '疑似人数')
- 
-    # wk.write(0,0,label = '省份')
-    # wk.write(0,1,label = '下属城市')
-    # wk.write(0,2,label = '死亡人数')
-    # wk.write(0,3,label = '治愈人数')
-    # wk.write(0,4,label = '确诊人数')
-    # wk.write(0,5,label = '疑似人数')
-    # k=1
-    # l=1
- 
-    # for i in ALL:
-    # #     print(i["provinceName"]+": "+"死亡人数："+str(i["deadCount"])+" 治愈人数："+str(i["curedCount"])+" 确诊人数："+str(i["confirmedCount"])+" 疑似人数："+str(i["suspectedCount"]))
-    #     ws.write(k,0,label = i["country"])
-    #     ws.write(k,1,label = i["provinceName"])
-    #     ws.write(k,2,label = i["deadCount"])
-    #     ws.write(k,3,label = i["curedCount"])
-    #     ws.write(k,4,label = i["confirmedCount"])
-    #     ws.write(k,5,label = i["suspectedCount"])
-    #     if i["country"]=="中国":
-    #         for j in i["cities"]:
-    #             wk.write(l,0,label = i["provinceName"])
-    #             wk.write(l,1,label = j["cityName"])
-    #             wk.write(l,2,label = j["deadCount"])
-    #             wk.write(l,3,label = j["curedCount"])
-    #             wk.write(l,4,label = j["confirmedCount"])
-    #             wk.write(l,5,label = j["suspectedCount"])
-    #             l+=1
-    # #             print(j["cityName"]+":\n 死亡人数："+str(j["deadCount"])+" 治愈人数："+str(j["curedCount"])+" 确诊人数："+str(j["confirmedCount"])+" 疑似人数："+str(j["suspectedCount"]))
-    #     k+=1
-    # #     print('\n')
-    # wb.save('新型肺炎%s时统计表.xls'%newTime)
-    # showinfo(title='成功', message='今日数据统计完成！') 
     
 # def fun():
 #     th=threading.Thread(target=cxk)
--- python/story_crawl.py
+++ python/story_crawl.py
@@ -1,80 +1,109 @@
 import requests
+from multiprocessing import Pool
 from bs4 import BeautifulSoup
 import re
 import time
 import os
+import random
 import logging
+import argparse
+
 
 # 通过下面的方式进行简单配置输出方式与日志级别
-logging.basicConfig(filename='story_crawl.log', level=logging.DEBUG)
+# create logger
+logger_name = "story_crawl"
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# create file handler
+filename = "story_crawl.log"
+fh = logging.FileHandler(filename)
+fh.setLevel(logging.INFO)
+
+# create formatter
+fmt = "%(message)s %(asctime)-15s %(levelname)s %(filename)s %(lineno)d %(process)d "
+formatter = logging.Formatter(fmt)
+
+# add handler and formatter to logger
+fh.setFormatter(formatter)
+logger.addHandler(fh)
 
 # 自定义headers请求
 ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) ' \
      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77' \
      ' Safari/537.36'
 headers = {'User-Agent': ua}
-timeout = 6
+timeout = random.randint(1,60)
+
+def page_crawling(url,params,i):      
+    if i> 1 :
+        params['page'] = i
+        logger.info('url={} page={}'.format(url,params['page'])) 
+    response = requests.get(url=url,params=params,timeout=timeout)
+    html = response.content.decode(response.apparent_encoding)
+    pattern = '(read.php\?tid=\d+&fpage=\d+)'
+    # logger.info(pattern)
+    # logger.info(html)
+    child_urls = re.findall(pattern,html)
+    if child_urls==[]:
+        pattern = '(html_data\/\d+\/\d+\/\d+\.html)'
+        child_urls = re.findall(pattern,html)
+    for child_url in child_urls:
+        curl=url.replace('thread.php','')+child_url
+        logger.info(curl)
+        crawling(curl)
+    timedelay = random.randint(3,60)
+    logger.info('time delay {}'.format(timedelay))
+    time.sleep(30)
 
 def crawling(url):
     pattern='<span id="subject_tpc">(?P<title>.*)</span>'
-    response = requests.get(url, headers=headers,timeout=timeout)
-    
+    response = requests.get(url, headers=headers,timeout=timeout)   
     # result = chardet.detect(response.content)
-    # logging.debug(result)
-    # logging.debug(response.status_code, response.reason)
+    # logger.info(result)
+    # logger.info(response.status_code, response.reason)
     html = response.content.decode(response.apparent_encoding)
     # title = re.search(pattern,html).group('title')
     soup = BeautifulSoup(html,features="lxml")
-    # logging.debug(soup.original_encoding)
+    # logger.info(soup.original_encoding)
     # title = soup.title.text
     title=soup.find(id='subject_tpc').text
-    # logging.debug(title)
+    # logger.info(title)
     content = soup.find(id='read_tpc').text
-    # logging.debug(content)
-    location='../story'
+    # logger.info(content)
+    
+
+    location='..\story'
     if not os.path.exists(location):
         os.mkdir(location)
     filename = title+'.md'
-    filename = os.path.join(location,filename)   
+    filename = re.sub('[\\\\/:*?\"<>|]','',filename)
+    filename = os.path.join(location,filename)      
     if os.path.exists(filename):
         return
-    # logging.debug(filename)
-    print(filename)
     with open(filename,'w+',encoding='utf8') as f:
         f.write(content)
         f.flush()
+        logger.info(filename) 
+    timedelay = random.randint(1,9)
+    time.sleep(timedelay)
 
 if __name__ == "__main__":
-    
-    params = {'fid':17,'type':2}
-    response = requests.get(url=url,params=params,timeout=timeout)
-    # logging.debug(response.status_code,response.reason)
-    # html_data/17/2002/4617751.html
-    child_urls = re.findall(r'html_data\/\d+\/\d+\/\d+\.html',response.text)
-    pages = re.search(r'Pages: +\d+\/(?P<page_count>\d+)',response.text)             
-    # logging.debug(pages)
-    # logging.debug(pages.group('page_count'))
-    page_count=int(pages.group('page_count'))
-    for i in range(1,page_count+1)[::-1]:
-        if i> 1 :
-            params['page'] = i 
-            # logging.debug(params)
+    parser = argparse.ArgumentParser(prog = 'story_crawl',description = '小说爬取')
+    parser.add_argument('url', help='input a url')
+    args = parser.parse_args()
+    if args.url:
+        url = args.url
+        logger.info(url) 
+        params = {'fid':17,'type':2}
         response = requests.get(url=url,params=params,timeout=timeout)
-        html = response.content.decode(response.apparent_encoding)
-        # logging.debug(url,params,i)
-        # logging.debug(response.status_code,response.reason)
-        pattern = '(read.php\?tid=\d+&fpage=\d+)'
-        # logging.debug(pattern)
-        # logging.debug(html)
-        child_urls = re.findall(pattern,html)
-        if child_urls==[]:
-            pattern = '(html_data\/\d+\/\d+\/\d+\.html)'
-            child_urls = re.findall(pattern,html)
-        # logging.debug(child_urls)
-        for child_url in child_urls:
-            curl=url.replace('thread.php','')+child_url
-            logging.debug(curl)
-            crawling(curl)
-        time.sleep(9)
+        child_urls = re.findall(r'html_data\/\d+\/\d+\/\d+\.html',response.text)
+        pages = re.search(r'Pages: +\d+\/(?P<page_count>\d+)',response.text)             
+        page_count=int(pages.group('page_count'))
+        pool = Pool()
+        multi_res = [pool.apply_async(page_crawling,args=(url,params,i))for i in range(1,page_count+1)]
+        random.shuffle(multi_res)
+        for res in multi_res:
+            res.get()   
 
 
