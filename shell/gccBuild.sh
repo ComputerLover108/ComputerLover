d8fc0c98b28df031f4617eb72bd28aa3e8f9669e
wkx <wkx@MyServer.ComputerLover.help>
2020-05-19 20:49:10 +0800

    clang build

--- shell/gccBuild.sh
+++ shell/gccBuild.sh
@@ -8,6 +8,7 @@ wget -c ftp://gcc.gnu.org/pub/gcc/releases/gcc-$VERSION/gcc-$VERSION.tar.gz &&
 tar xfz gcc-$VERSION.tar.gz &&
 cd gcc-$VERSION &&
 ./contrib/download_prerequisites &&
+VERSION=${VERSION%.*} && 
 #CC=clang ./configure --disable-multilib --prefix=/opt/gcc6.3 &&
 CC=clang ./configure --prefix=/opt/gcc$VERSION --enable-checking=release --enable-languages=c,c++ --disable-multilib --disable-bootstrap 1>../gccBuild.log 2>../gccBuildError.log &&
 make -s -j $JOBS 1>>../gccBuild.log 2>>../gccBuildError.log &&
